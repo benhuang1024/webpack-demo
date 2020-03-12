1d7653ef1a5a78445458df09c99a963e4a1bdcdc
benhuang <benhuang1024@gmail.com>
2020-03-13 00:11:05 +0800

    add plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     // JavaScript 执行入口文件
@@ -6,7 +7,7 @@ module.exports = {
     output: {
         // 把所有依赖的模块合并输出到一个 bundle.js 文件
         filename: 'bundle.js',
-        // 输出文件都放到 dist 目录下
+        // 把输出文件都放到 dist 目录下
         path: path.resolve(__dirname, './dist'),
     },
     module: {
@@ -14,8 +15,17 @@ module.exports = {
             {
                 // 用正则去匹配要用该 loader 转换的 CSS 文件
                 test: /\.css$/,
-                use: ['style-loader','css-loader'],
+                use: ExtractTextPlugin.extract({
+                    // 转换 .css 文件需要使用的 Loader
+                    use: ['css-loader'],
+                }),
             }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            // 从 .js 文件中提取出来的 .css 文件的名称
+            filename: `[name]_[md5:contenthash:hex:8].css`,
+        }),
+    ]
 };
